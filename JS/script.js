2862cb38ca0b4a0bb5a4f22c8109a263fafa5807
alep-2000 <alessiopiras056@gmail.com>
2023-05-16 16:54:38 +0200

    [FEAT STYLE]Exercise finisched

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// NOME
+let name = prompt("Qual'è il tuo nome?");
+// COGNOME
+let surname = prompt("Qual'è il tuo cognome?");
+// COLORE
+let color = prompt("Qual'è il tuo colore preferito?");
+// NUMERO
+let num =21;
+// COMPLETO
+let completo = name + surname + color + num;
+document .getElementById('text') .innerHTML= completo
