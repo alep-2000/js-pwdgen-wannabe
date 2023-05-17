5ef105409cadb362debd29527a505ddd9bd2bedd
alep-2000 <alessiopiras056@gmail.com>
2023-05-17 09:43:27 +0200

    [FEAT STYLE]Add proper way

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
 // NOME
-let name = prompt("Qual'è il tuo nome?");
+let name = prompt("Qual è il tuo nome?");
 // COGNOME
-let surname = prompt("Qual'è il tuo cognome?");
+let surname = prompt("Qual è il tuo cognome?");
 // COLORE
-let color = prompt("Qual'è il tuo colore preferito?");
+let color = prompt("Qual è il tuo colore preferito?");
 // NUMERO
 let num =21;
 // COMPLETO
-let completo = name + surname + color + num;
-document .getElementById('text') .innerHTML= completo
+let completo = `${name}${surname}${color}${num}`;
+document .getElementById('text') .innerHTML= completo;
+
